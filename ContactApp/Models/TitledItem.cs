74394e9fd9b441586628dbd07000097d9efd4f20
nezhadian <yasin.ebr.nezh@gmail.com>
2023-06-27 16:02:38 +0430

    Titled Item Model Added

---
+++ ContactApp/Models/TitledItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactApp.Models
+{
+    class TitledItem : ModelsBase
+    {
+        private string _Title;
+        public string Title
+        {
+            get => _Title;
+            set
+            {
+                _Title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private object _value;
+        public object Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
