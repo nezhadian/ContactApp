522b4fc877ad51f68af339dd8a0b9567d6a8f284
nezhadian <yasin.ebr.nezh@gmail.com>
2023-06-27 16:38:43 +0430

    Titled Item View Model Implemented

---
+++ ContactApp/ViewModels/TitledItemViewModel.cs
@@ -0,0 +1,31 @@
+﻿using ContactApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactApp.ViewModels
+{
+    class TitledItemViewModel : CollectionViewModel<TitledItem>
+    {
+
+        private string _Title;
+        public string Title
+        {
+            get => _Title;
+            set
+            {
+                _Title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public TitledItemViewModel(ObservableCollection<TitledItem> collection)
+        {
+            ItemsCollection = collection;
+        }
+
+    }
+}
