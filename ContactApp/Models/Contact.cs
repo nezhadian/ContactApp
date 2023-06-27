4a44d13329523e66e0b46cd7428f21d43695ac7f
nezhadian <yasin.ebr.nezh@gmail.com>
2023-06-28 00:35:24 +0430

    Contact :  IsEmailsEmpty And IsNumbersEmpty Added

--- ContactApp/Models/Contact.cs
+++ ContactApp/Models/Contact.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -15,6 +16,8 @@ namespace ContactApp.Models
         #region Private Fields
         private string _firstName;
         private string _lastName;
+        private ObservableCollection<TitledItem> _emailsCollection;
+        private ObservableCollection<TitledItem> _numbersCollection;
         private bool _isFavorite;
         private Brush _profilePicture;
         #endregion
@@ -38,8 +41,63 @@ namespace ContactApp.Models
             }
         }
 
-        public ObservableCollection<TitledItem> EmailsCollection { get; set; }
-        public ObservableCollection<TitledItem> NumbersCollection { get; set; }
+        public ObservableCollection<TitledItem> EmailsCollection
+        {
+            get => _emailsCollection;
+            set
+            {
+                if (value == null)
+                    return;
+
+                if (_emailsCollection != null)
+                    _emailsCollection.CollectionChanged -= OnEmailsCollectionChanged;
+
+                value.CollectionChanged += OnEmailsCollectionChanged;
+                OnEmailsCollectionChanged(null, null);
+
+                _emailsCollection = value;
+                OnPropertyChanged();
+
+            }
+        }
+        public ObservableCollection<TitledItem> NumbersCollection
+        {
+            get => _numbersCollection;
+            set
+            {
+                if (value == null)
+                    return;
+
+                if (_numbersCollection != null)
+                    _numbersCollection.CollectionChanged -= OnNumbersCollectionChanged;
+
+                _numbersCollection = value;
+
+                OnPropertyChanged();
+                value.CollectionChanged += OnNumbersCollectionChanged;
+                OnNumbersCollectionChanged(null, null);
+            }
+        }
+
+        public bool IsEmailsEmpty
+        {
+            get => EmailsCollection == null || EmailsCollection.Count == 0;
+        }
+        public bool IsNumbersEmpty
+        {
+            get => NumbersCollection == null || NumbersCollection.Count == 0;
+        }
+
+        private void OnEmailsCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            OnPropertyChanged(nameof(IsEmailsEmpty));
+        }
+        private void OnNumbersCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            OnPropertyChanged(nameof(IsNumbersEmpty));
+
+        }
+
 
         public bool IsFavorite
         {
