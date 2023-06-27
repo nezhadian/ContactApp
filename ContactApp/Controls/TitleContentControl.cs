3e031db230a296d76df7710f5ae88b05e6cf6ff5
nezhadian <yasin.ebr.nezh@gmail.com>
2023-06-27 22:16:11 +0430

    TitleContentControl : Added

---
+++ ContactApp/Controls/TitleContentControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ContactApp.Controls
+{
+    class TitleContentControl : ContentControl
+    {
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(TitleContentControl), new PropertyMetadata());
+    }
+}
