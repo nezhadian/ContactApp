98132faedebb6733b6467c81082433a50d33a282
nezhadian <yasin.ebr.nezh@gmail.com>
2023-07-02 16:40:17 +0430

    Contact View Model : Edit Command Added and Implemented

--- ContactApp/ViewModels/ContactViewModel.cs
+++ ContactApp/ViewModels/ContactViewModel.cs
@@ -1,18 +1,35 @@
 ﻿using ContactApp.Models;
+using ContactApp.Pages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ContactApp.ViewModels
 {
     public class ContactViewModel : CollectionViewModel<Contact>
     {
         public static CustomCommand SaveCommand { set; get; }
+        public static CustomCommand EditSelectedItem { set; get; }
+        static EditContactItemPage EditPage;
+
         public ContactViewModel()
         {
             SaveCommand = new CustomCommand(OnSaveExecuted, SaveCanExecute);
+            EditSelectedItem = new CustomCommand(OnEditSelectedItemExecuted, EditSelectedItemCanExecute);
+            EditPage = new EditContactItemPage();
+        }
+
+        private bool EditSelectedItemCanExecute(object parameter)
+        {
+            return SelectedItem != null;
+        }
+        private void OnEditSelectedItemExecuted(object parameter)
+        {
+            EditPage.TargetContact = SelectedItem;
+            MainWindow.NavigateToPage(EditPage);
         }
 
         private bool SaveCanExecute(object parameter)
