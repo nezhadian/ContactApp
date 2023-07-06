3db9f24fb6f98610b5b8977079a05b096acac3cf
nezhadian <yasin.ebr.nezh@gmail.com>
2023-07-06 12:05:07 +0430

    HomePage View Model : Dummy Data

--- ContactApp/ViewModels/HomePageViewModel.cs
+++ ContactApp/ViewModels/HomePageViewModel.cs
@@ -13,7 +13,36 @@ namespace ContactApp.ViewModels
 
         public HomePageViewModel()
         {
-            ContactViewModel = new ContactViewModel();
+            var emails = new TitledItemViewModel()
+            {
+                ItemsCollection = new System.Collections.ObjectModel.ObservableCollection<Models.TitledItem>()
+                {
+                    new Models.TitledItem(){Title = "Work",Value = "example@gmail.com"},
+                    new Models.TitledItem(){Title = "Persoal",Value = "yasin@yahoo.net"},
+                    new Models.TitledItem(){Title = "Home",Value = "matin@hotemail.com"},
+                    new Models.TitledItem(){Title = "Office",Value = "ali@protonEmail.com"},
+                }
+            };
+
+            var numbers = new TitledItemViewModel()
+            {
+                ItemsCollection = new System.Collections.ObjectModel.ObservableCollection<Models.TitledItem>()
+                {
+                    new Models.TitledItem(){Title = "Personal",Value = "+98 991 852 5922"},
+                    new Models.TitledItem(){Title = "Home",Value = "+98 914 595 7929"},
+                    new Models.TitledItem(){Title = "Other Home",Value = "+98 914 334 9374"},
+                }
+            };
+            ContactViewModel = new ContactViewModel()
+            {
+                ItemsCollection = new System.Collections.ObjectModel.ObservableCollection<Models.Contact>
+                {
+                    new Models.Contact(){LastName = "Yasini" ,FirstName = "Yasin" ,ProfilePicture = new SolidColorBrush(Colors.Violet) ,EmailsViewModel = emails,NumbersViewModel = numbers},
+                    new Models.Contact(){LastName = "Matini" ,FirstName = "Matin" ,ProfilePicture = new SolidColorBrush(Colors.Goldenrod) },
+                    new Models.Contact(){LastName = "Ahmadi" ,FirstName = "Ahmad" ,ProfilePicture = new SolidColorBrush(Colors.IndianRed) ,EmailsViewModel = emails},
+                    new Models.Contact(){LastName = "Mamadi" ,FirstName = "Mamad" ,ProfilePicture = new SolidColorBrush(Colors.HotPink) ,NumbersViewModel = numbers},
+                }
+            };
         }
     }
 }
