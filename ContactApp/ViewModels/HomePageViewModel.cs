50a584484522b9c515d7aaa611d5e487fe0ecdda
nezhadian <yasin.ebr.nezh@gmail.com>
2023-07-06 12:00:27 +0430

    HomePage : Converted To ViewModel

---
+++ ContactApp/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ContactApp.ViewModels
+{
+    class HomePageViewModel : ViewModelBase
+    {
+        public ContactViewModel ContactViewModel { set; get; }
+
+        public HomePageViewModel()
+        {
+            ContactViewModel = new ContactViewModel();
+        }
+    }
+}
