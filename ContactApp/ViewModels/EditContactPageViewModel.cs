711b45591367e70dc0cc520d4d356f544980038c
nezhadian <yasin.ebr.nezh@gmail.com>
2023-07-06 12:01:08 +0430

    EditContactPage : Converted To ViewModel

---
+++ ContactApp/ViewModels/EditContactPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ContactApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactApp.ViewModels
+{
+    class EditContactPageViewModel : ViewModelBase
+    {
+        private Contact _targetContact;
+        public Contact TargetContact
+        {
+            get => _targetContact;
+            set
+            {
+                _targetContact = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
